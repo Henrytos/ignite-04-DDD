daaf8b74c7107c14fc959c69afd5933abd9464f5
Henry <franzhenry46@gmail.com>
2024-06-22 15:30:18 -0300

    fix: create entity notification in sub domain notification

---
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,45 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+
+export interface NotificationProps {
+    recipientId: string
+    title: string
+    content: string
+    createdAt: Date
+    readAt?: Date
+}
+
+export class Notification extends Entity<NotificationProps> {
+
+    get title() {
+        return this.props.title
+    }
+
+    get content() {
+        return this.props.content
+    }
+
+    get recipientId() {
+        return this.props.recipientId
+    }
+
+    get createdAt() {
+        return this.props.createdAt
+    }
+
+    get readAt() {
+        return this.props.readAt
+    }
+
+    static create(props: Optional<NotificationProps, 'createdAt'>, id?: UniqueEntityID) {
+
+        const notification = new Notification({
+            createdAt: props.createdAt ?? new Date(),
+            ...props
+        }, id)
+
+        return notification
+    }
+
+}
